5097548a0307f3ddff7722daf94f3e701b59153f
ajay <ajay@bhandare.com>
2023-10-16 23:39:37 +0530

    javascript Adding

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    // alert("sucessfully");
+    var element = document.getElementById("a");
+    var elementt = document.getElementById("aa");
+    var elementtt = document.getElementById("aaa");
+    var elementttt = document.getElementById("aaaa");
+    var elementtttt= document.getElementById("aaaaa");
+    var elements= document.getElementById("nav");
+    element.classList.add("b");
+    elementt.classList.add("b");
+    elementtt.classList.add("b");
+    elementttt.classList.add("b");
+    elementtttt.classList.add("b");
+    elements.classList.add("mystyle");
+  }
+  
